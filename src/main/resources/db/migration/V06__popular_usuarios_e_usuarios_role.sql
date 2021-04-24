dc18c74ef42769526985f92b6d1a11f2eaf82c70
Antônio Vieira <alvieirajr@gmail.com>
2021-04-24 12:47:11 -0300

    Scripts de usuarios.

--- src/main/resources/db/migration/V06__popular_usuarios_e_usuarios_role.sql
+++ src/main/resources/db/migration/V06__popular_usuarios_e_usuarios_role.sql
@@ -1,7 +1,8 @@
-
+delete from usuario_role;
+delete from usuario;
 insert into usuario(codigo, PASSWORD, USERNAME) values(1, '$2a$10$bnC26zz//2cavYoSCrlHdecWF8tkGfPodlHcYwlACBBwJvcEf0p2G', '123');
 insert into usuario(codigo, PASSWORD, USERNAME) values(2, '$2a$10$bnC26zz//2cavYoSCrlHdecWF8tkGfPodlHcYwlACBBwJvcEf0p2G', '1234');
 insert into usuario_ROLE(codigoUsuario, ROLE) values(1, 'ADMIN');
 insert into usuario_ROLE(codigoUsuario, ROLE) values(1, 'PREMIUM_MEMBER');
 insert into usuario_ROLE(codigoUsuario, ROLE) values(2, 'PREMIUM_MEMBER');
-commit
+commit;
